91c902ca831443be02d8c75bce5ef616f46f1b47
Elliad <alstmd3@naver.com>
2016-05-08 02:06:50 +0900

    make Model, ModelTest, ControllerTest class Singleton pattern applied in Model. Some model tests applied.

---
+++ src/controller/ControllerTest.java
@@ -0,0 +1,7 @@
+package controller;
+
+/**
+ * Created by Elliad on 2016-05-08.
+ */
+public class ControllerTest {
+}
---
+++ src/model/ModelTest.java
@@ -0,0 +1,25 @@
+package model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Elliad on 2016-05-08.
+ */
+public class ModelTest {
+    @Test
+    public void singletonTest() {
+        assertNotNull(Model.getModel());
+    }
+
+    @Test
+    public void fileReadTest() {
+        assertNotNull(Model.fileA);
+        assertNotNull(Model.fileB);
+    }
+
+    @Test
+    public void fileWriteTest() {
+
+    }
+}

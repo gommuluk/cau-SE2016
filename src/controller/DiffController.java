80151365bd6a680ef2af9552f293c7a097cf3e20
Elliad <alstmd3@naver.com>
2016-05-08 21:29:16 +0900

    diff controller applied

---
+++ src/controller/DiffController.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class DiffController {
+    public boolean compare(){
+        return true;
+    }
+}
--- src/controller/DiffControllerTest.java
+++ src/controller/DiffControllerTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 /**
  * Created by Elliad on 2016-05-08.
  */
-public class ControllerTest {
+public class DiffControllerTest {
     @Test
     public void test() {
         /* 이와 같은 형식으로 작성하면 됨 */

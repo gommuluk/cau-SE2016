1ee060450d42a6a9e89bb52bf85b7e59e88a483a
Elliad <alstmd3@naver.com>
2016-05-11 15:04:58 +0900

    btn action method moves to TabPaneController class from UndecoratedRootController class

--- src/controller/UndecoratedRootSceneController.java
+++ src/controller/UndecoratedRootSceneController.java
@@ -228,21 +228,4 @@ public class UndecoratedRootSceneController {
         return (draggableZoneY || draggableZoneX);
     }
 
-
-    @FXML //TODO 나중에 (Toolbar)Pane을 나눠야 됩니다.
-          //TODO TabPane으로 옮겨야 합니다.
-    private void onTBBtnFileOpenClicked(ActionEvent event) {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Resource File");
-        fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
-                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
-                new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"),
-                new FileChooser.ExtensionFilter("All Files", "*.*"));
-        //File selectedFile = fileChooser.showOpenDialog();
-        //if (selectedFile != null) {
-        //    mainStage.display(selectedFile);
-        //}
-    }
-
 }

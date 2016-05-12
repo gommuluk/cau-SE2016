05aa70e8e6b06f79f75c86cc8b003e522eb027c3
nbkwon <cccang8@naver.com>
2016-05-13 03:00:29 +0900

    Change Filemodel for efficienty. 효율성을 위해 Filemodel 구조를 변경하였습니다

--- src/model/FileModel.java
+++ src/model/FileModel.java
@@ -10,10 +10,8 @@ import java.io.FileReader;
  */
 public class FileModel {
     private static FileModel instance;
-    private static File fileL;
-    private static File fileR;
-    private static String stringL = "";
-    private static String stringR = "";
+    private static File file;
+    private static String string = "";
     private FileModel() { // 기본 생성자
     }
     public static FileModel getModel() { // 1개의 객체를 유지하기 위한 싱글톤
@@ -22,11 +20,11 @@ public class FileModel {
         return instance;
     }
 
-    public boolean readFileR(String fileRPath) { // 파일명 받기 및 읽기.
-        fileR= new File(fileRPath);
-        try(Scanner in = new Scanner(new FileReader(fileRPath)))
+    public boolean readFile(String filePath) { // 파일명 받기 및 읽기.
+        file= new File(filePath);
+        try(Scanner in = new Scanner(new FileReader(filePath)))
         {
-            stringR = in.useDelimiter("\\Z").next();
+            string = in.useDelimiter("\\Z").next();
         } catch (FileNotFoundException e) {
 
             e.printStackTrace();
@@ -34,40 +32,15 @@ public class FileModel {
         }
         return true;
     }
-    public boolean readFileL(String fileLPath) { // 파일명 받기 및 읽기.
-        fileL = new File(fileLPath);
-        try(Scanner in = new Scanner(new FileReader(fileLPath)))
-        {
-            stringL = in.useDelimiter("\\Z").next();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
-    public String getStringR()
+    public String getString()
     {
-        return stringR;
+        return string;
     }
 
-    public String getStringL()
-    {
-        return stringL;
-    }
 
-    public boolean writeFileL(String fileLPath) { // 파일명 받기 및 읽기
-        try(  PrintWriter out = new PrintWriter(fileLPath) ){
-            out.print(stringL);
-            out.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
-    public boolean writeFileR(String fileRPath) { // 파일명 받기 및 읽기
-        try(  PrintWriter out = new PrintWriter(fileRPath) ){
-            out.print(stringR);
+    public boolean writeFile(String filePath) { // 파일명 받기 및 읽기
+        try(  PrintWriter out = new PrintWriter(filePath) ){
+            out.print(string);
             out.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();

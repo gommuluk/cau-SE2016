3c71977b651f971a87ec1b1664d79c95b61d27ad
HyominNam <skagbghk@gmail.com>
2016-05-20 03:20:19 +0900

    make FileExplorer class

---
+++ src/controller/FileExplorer.java
@@ -0,0 +1,52 @@
+package controller;
+
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+/**
+ * Created by 효민 on 2016-05-20.
+ */
+public class FileExplorer {
+
+//    Button btn;
+//    public FileExplorer(Button btn){
+//        this.btn = btn;
+//    }
+    public File getSaveDialog(Button btn){
+
+        Stage s = (Stage) btn.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+
+        fileChooser.setTitle("Save File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
+                new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"),
+                new FileChooser.ExtensionFilter("All Files", "*.*"));
+
+        File file = fileChooser.showSaveDialog(s);
+        return file;
+    }
+
+    public File getLoadDialog(Button btn){
+
+        Stage s = (Stage) btn.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+
+        fileChooser.setTitle("Open File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
+                new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"),
+                new FileChooser.ExtensionFilter("All Files", "*.*"));
+
+        File selectedFile = fileChooser.showOpenDialog(s);
+        return selectedFile;
+    }
+}
+
+
+

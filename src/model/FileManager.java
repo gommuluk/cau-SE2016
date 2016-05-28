40538a67f122955c06250ecd68a0595fc49eb8f5
lee senghyun <leesnhyun@naver.com>
2016-05-28 19:32:06 +0900

    view modified

--- src/model/FileManager.java
+++ src/model/FileManager.java
@@ -135,7 +135,10 @@ public class FileManager implements FileManagerInterface {
 
     @Override
     public void clickLine(int lineNum) {
-        if (isComparing) fileModelL.clickLine(lineNum);//왼쪽이든 오른쪽이든 상관없음! 일부러 사이드 검사를 안해줬습니다
+        if (isComparing){
+            fileModelL.clickLine(lineNum);//왼쪽이든 오른쪽이든 상관없음! 일부러 사이드 검사를 안해줬습니다
+            fileModelR.clickLine(lineNum);
+        }
 
     }
 

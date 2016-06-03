1ccd82f1fe8091a45b524d321e284673a40bf47f
nbkwon <cccang8@naver.com>
2016-06-03 23:31:51 +0900

    Add dependency - apache 어쩌고 recover testcases

---
+++ src/test/java/model/FileManagerTest.java
@@ -0,0 +1,119 @@
+package model;
+
+import org.junit.Before;
+import org.junit.Test;
+import model.FileModel;
+import model.FileManager;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+/**
+ * Created by ano on 2016. 5. 18..
+ */
+public class FileManagerTest {
+    private static FileManagerInterface fileManager;
+
+    @Before
+    public void setupTest () throws FileNotFoundException, UnsupportedEncodingException, LeftEditorFileCanNotCompareException, RightEditorFileCanNotCompareException
+    {
+        //fileManager = FileManager.getFileManager();
+        fileManager = FileManager.getFileManagerInterface();
+        fileManager.loadFile("test files/AA.txt",FileManagerInterface.SideOfEditor.Left);
+        fileManager.loadFile("test files/BB.txt",FileManagerInterface.SideOfEditor.Right);
+        fileManager.setCompare();
+
+    }
+
+    @Test
+    public void buildArrayLCSTest() {
+        int[][] arr = {{0, 0, 0, 0, 0}, {0, 1, 1, 1, 1}, {0, 1, 2, 2, 2}, {0, 1, 2, 2, 3}, {0, 1, 2, 3, 3}};
+
+
+        //L이 a b c d, R이 a b d c 일떄의 DP 테이블
+        assertArrayEquals(arr, fileManager.getArrayLCS()); //테이블 검사
+    }
+    /*
+    @Test
+    public void backTrackingLCSTest(){
+        ArrayList<Line> testLineArrayListL = new ArrayList<Line>();
+        ArrayList<Line> testLineArrayListR = new ArrayList<Line>();
+        ArrayList<Block> testBlockArrayList = new ArrayList<Block>();
+
+        testBlockArrayList.add(new Block(4, 5));
+        testBlockArrayList.add(new Block(2, 3));
+        assertEquals(testBlockArrayList.size(),Line.getBlockArray().size());//블럭 크기 같은지 검사
+        for (int i = 0; i < testBlockArrayList.size(); i++) {
+            assertEquals(testBlockArrayList.get(i).startLineNum, Line.getBlockArray().get(i).startLineNum);
+            assertEquals(testBlockArrayList.get(i).endLineNum, Line.getBlockArray().get(i).endLineNum);//블럭 같게 생겼는지 검사
+        }
+
+        testLineArrayListL.add(new Line("a",-1,false));
+        testLineArrayListL.add(new Line("b",-1,false));
+        testLineArrayListL.add(new Line("",1,true));
+        testLineArrayListL.add(new Line("c",-1,false));
+        testLineArrayListL.add(new Line("d",0,false));
+        testLineArrayListR.add(new Line("a",-1,false));
+        testLineArrayListR.add(new Line("b",-1,false));
+        testLineArrayListR.add(new Line("d",1,false));
+        testLineArrayListR.add(new Line("c",-1,false));
+        testLineArrayListR.add(new Line("",0,true));
+        for (int i = 0; i < testLineArrayListR.size(); i++) {
+
+
+            assertEquals(testLineArrayListR.get(i).getContent(false),fileManager.getLineArrayList(FileManagerInterface.SideOfEditor.Right).get(i).getContent(false));
+            assertEquals(testLineArrayListR.get(i).getBlockIndex(),fileManager.getLineArrayList(FileManagerInterface.SideOfEditor.Right).get(i).getBlockIndex());
+            assertEquals(testLineArrayListR.get(i).getIsWhiteSpace(),fileManager.getLineArrayList(FileManagerInterface.SideOfEditor.Right).get(i).getIsWhiteSpace());
+            assertEquals(testLineArrayListL.get(i).getContent(false),fileManager.getLineArrayList(FileManagerInterface.SideOfEditor.Left).get(i).getContent(false));
+            assertEquals(testLineArrayListL.get(i).getBlockIndex(),fileManager.getLineArrayList(FileManagerInterface.SideOfEditor.Left).get(i).getBlockIndex());
+            assertEquals(testLineArrayListL.get(i).getIsWhiteSpace(),fileManager.getLineArrayList(FileManagerInterface.SideOfEditor.Left).get(i).getIsWhiteSpace());
+
+            //텍스트 어레이 같게 생겼는지 검사
+        }
+
+    }
+    */
+    @Test
+    public void LeftToRightMergeTest()
+    {
+        //abcd ffcd 의 경우
+        ArrayList<Line> testLineArrayListL = new ArrayList<Line>();
+        ArrayList<Line> testLineArrayListR = new ArrayList<Line>();
+        testLineArrayListL.add(new Line("a",-1,false));
+        testLineArrayListL.add(new Line("b",-1,false));
+        testLineArrayListL.add(new Line("c",-1,false));
+        testLineArrayListL.add(new Line("d",-1,false));
+        testLineArrayListR.add(new Line("a",-1,false));
+        testLineArrayListR.add(new Line("b",-1,false));
+        testLineArrayListR.add(new Line("c",-1,false));
+        testLineArrayListR.add(new Line("d",-1,false));
+        fileManager.clickLine(2);
+        fileManager.clickLine(4);
+        fileManager.merge(FileManagerInterface.SideOfEditor.Right);
+        for (int i = 0; i < testLineArrayListL.size(); i++) {
+            System.out.print(i);
+            System.out.println(fileManager.getLineArrayList(FileManagerInterface.SideOfEditor.Left).get(i).getContent(true) + " Left ");
+            assertEquals(testLineArrayListL.get(i).getContent(true),fileManager.getLineArrayList(FileManagerInterface.SideOfEditor.Left).get(i).getContent(true));
+            assertEquals(testLineArrayListL.get(i).getBlockIndex(),fileManager.getLineArrayList(FileManagerInterface.SideOfEditor.Left).get(i).getBlockIndex());
+            assertEquals(testLineArrayListL.get(i).getIsWhiteSpace(),fileManager.getLineArrayList(FileManagerInterface.SideOfEditor.Left).get(i).getIsWhiteSpace());
+//텍스트 어레이 같게 생겼는지 검사
+        }
+        for (int i = 0; i < testLineArrayListR.size(); i++) {
+
+            System.out.print(i);
+            System.out.println(fileManager.getLineArrayList(FileManagerInterface.SideOfEditor.Right).get(i).getContent(true) + " Right");
+            assertEquals(testLineArrayListR.get(i).getContent(true),fileManager.getLineArrayList(FileManagerInterface.SideOfEditor.Right).get(i).getContent(true));
+            assertEquals(testLineArrayListR.get(i).getBlockIndex(),fileManager.getLineArrayList(FileManagerInterface.SideOfEditor.Right).get(i).getBlockIndex());
+            assertEquals(testLineArrayListR.get(i).getIsWhiteSpace(),fileManager.getLineArrayList(FileManagerInterface.SideOfEditor.Right).get(i).getIsWhiteSpace());
+//텍스트 어레이 같게 생겼는지 검사
+        }
+    }
+
+    @Test
+    public void RightToLeftMergeTest()
+    {
+
+    }
+
+}
---
+++ src/test/java/model/FileModelTest.java
@@ -0,0 +1,71 @@
+package model;
+
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.StringProperty;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import model.FileModel;
+import model.FileManager;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Elliad on 2016-05-08.
+ */
+public class FileModelTest {
+    @Test
+    public void fileReadTest() throws FileNotFoundException, UnsupportedEncodingException{
+        FileModel testFileModel = new FileModel();
+        ArrayList<String> testArraylist = new ArrayList<String>();
+        testArraylist.add("asdf\n");
+        testArraylist.add("asdf");
+        //assertTrue(testFileModel.readFile("A.txt"));
+        //assertEquals(testFileModel.getStringArrayList(),testArraylist);
+
+
+    }
+
+    @Test
+    public void fileWriteTest() throws FileNotFoundException, UnsupportedEncodingException{
+
+        FileModel testFileModel = new FileModel();
+        //assertTrue(testFileModel.readFile("A.txt"));
+        //assertTrue(testFileModel.writeFile());
+        ArrayList<String> testArraylist = new ArrayList<String>();
+        try(Scanner in = new Scanner(new FileReader("test files/AA.txt")))
+        {
+            String tempString = "";
+            while(in.hasNext()) {
+                tempString = in.next(); //임시 텍스트에 개행을 제외한 한 줄을 불러온다
+                if(in.hasNext()) tempString +="\n"; //다음 줄이 없을 때는 개행을 추가하지 않는다.
+                testArraylist.add(tempString);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        //assertEquals(testFileModel.getStringArrayList(),testArraylist);
+
+    }
+
+    @Test
+    //미구현
+    public void getStatusTest()
+    {
+        /*
+        FileModel testFileModel = new FileModel();
+        String testStatus = "Ready(No file is loaded)";
+        //assertEquals(testFileModel.getStatus(), new ReadOnlyStringWrapper(testStatus));
+        //assertTrue(testFileModel.readFile("A.txt"));
+        //testStatus = "File Loaded Successfully";
+        //assertEquals(testFileModel.getStatus(),new ReadOnlyStringWrapper(testStatus));
+        //testFileModel.writeFile("AA.txt");
+        testStatus = "File Written successfully";
+        //assertEquals(testFileModel.getStatus(),new ReadOnlyStringWrapper(testStatus));
+    */
+    }
+
+}

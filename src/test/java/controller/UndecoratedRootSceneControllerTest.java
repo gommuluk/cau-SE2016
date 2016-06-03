e2291f7521d0c7e32d1cfdf4ede6cd6382b619aa
lee senghyun <leesnhyun@naver.com>
2016-06-03 13:52:28 +0900

    temp svae

---
+++ src/test/java/controller/UndecoratedRootSceneControllerTest.java
@@ -0,0 +1,16 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by SH on 2016-06-03.
+ */
+
+public class UndecoratedRootSceneControllerTest {
+
+    public UndecoratedRootSceneControllerTest(){
+
+
+    }
+
+}
